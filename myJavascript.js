a4ddcd70d7f19f2880f28c49c6e8be3c5c9d1b7c
ytlisjsu <70075043+ytlisjsu@users.noreply.github.com>
2021-05-21 22:06:57 -0700

    Add files via upload

---
+++ myJavascript.js
@@ -0,0 +1,16 @@
+function initViz() {
+	var containerDiv = document.getElementById("tableauViz"),
+	url = "https://public.tableau.com/views/SP500_weight/SP500weight?:language=en&:display_count=y&publish=yes&:toolbar=n&:origin=viz_share_link"
+
+	var viz 
+
+}
+
+const searchBar = document.getElementById('searchBar');
+
+console.log(searchBar);
+
+searchBar.addEventListener('keyUp', (e) => {
+	console.log(e);
+});
+
